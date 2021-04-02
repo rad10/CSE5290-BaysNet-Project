c0aab61167ec53ecde7207b49186933fa8aa8b81
RadioLogic <ncottrellweb@gmail.com>
2021-04-02 13:53:29 -0400

    Added commented test functions using special args

--- Test.py
+++ Test.py
@@ -41,9 +41,21 @@ print(enumeration_all(JohnCall, MaryCall, M=True))
 print(enumeration_ask(MaryCall))
 
 
+# My tests on special args
+# def func(**args: bool):
+#     print(type(args))
+#     print(args)
+#     print(len(args))
+#     print(bool(args))
 
-# print(Alarm())
-# print(Alarm({"B": False, "E": True}))
 
-print(enumeration_all([MaryCall], {"M": True}))
-print(enumeration_ask([MaryCall], {}))
+# def sfunc(query: str, *given: str, **observed: bool):
+#     print(query, given, observed)
+#     print(type(given), type(observed))
+#     print(bool(given), bool(observed))
+
+
+# func()
+# sfunc("A")
+# func(A=True, B=False, C="Hello", D=None)
+# sfunc("A", "B", "C", "D", B=True, C=True, D=False)
